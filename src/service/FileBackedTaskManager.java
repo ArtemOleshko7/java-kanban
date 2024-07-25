8d4b8f4f26c53cd7ea41e5882fb2acaf909f795b
sergeyvoronin <sesvoronin@detmir.ru>
2024-07-26 00:15:30 +0300

    Mister Dick

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -8,6 +8,8 @@ import model.Task;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 
@@ -15,13 +17,17 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     public final File taskManagerFile;
 
-    public FileBackedTaskManager(HistoryManager historyManager, String fileName) {
+    public FileBackedTaskManager(HistoryManager historyManager, String fileName) throws IOException {
         super(historyManager);
         this.taskManagerFile = new File(fileName);
+        Path path = Path.of(fileName);
+        if (!Files.exists(path)){
+            Files.createFile(path);
+        }
         loadFromFile(); // Загружаем данные из файла при создании
     }
 
-    public FileBackedTaskManager(String fileName) {
+    public FileBackedTaskManager(String fileName) throws IOException {
         this(new InMemoryHistoryManager(), fileName);
     }
 
@@ -54,7 +60,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
                 Epic epic = new Epic(id, name, description, status);
                 if (parts.length > SUBTASK_ID_INDEX) {
                     for (int i = SUBTASK_ID_INDEX; i < parts.length; i++) {
-                        int subtaskId = Integer.parseInt(parts[i].trim());
+                        int subtaskId = Integer.parseInt(parts[i].substring(1,2).trim());
                         Subtask subtask = taskManager.getSubtask(subtaskId);
                         if (subtask != null) {
                             epic.addSubtaskId(subtaskId);
